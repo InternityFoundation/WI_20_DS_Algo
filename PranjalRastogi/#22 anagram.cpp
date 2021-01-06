2690080c832ed488c1468ef4cf7db0bb2b955d2a
pranjalrusty <44286973+pranjalrusty@users.noreply.github.com>
2021-01-06 21:13:49 +0530

    Create #22 anagram.cpp

---
+++ PranjalRastogi/#22 anagram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p)
+    {
+        map <char, int> m;
+      int n = s.size();
+      int left = 0, right = 0;
+      int counter = p.size();
+      vector <int> ans;
+      for(int i = 0; i < p.size(); i++) m[p[i]]++;
+      for(int right = 0; right < n; right++){
+         if(m.find(s[right]) != m.end() && m[s[right]]){
+            m[s[right]]--;
+            counter--;
+            if(counter == 0)ans.push_back(left);
+         } else {
+            while(left<right){
+               if(m.find(s[left]) != m.end()) {
+                  counter++;
+                  m[s[left]]++;
+               }
+               left++;
+               if(m.find(s[right]) != m.end() && m[s[right]]){
+                  right--;
+                  break;
+               }
+            }
+            if(m.find(s[left])==m.end())left = right + 1;
+         }
+      }
+      return ans;
+   }
+        
+};
