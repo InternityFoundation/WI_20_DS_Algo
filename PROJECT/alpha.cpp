27799d147d7ba35ce01cf7eda6dcdf27102c532a
Sandeep kumar <sandeepkumar0702000@gmail.com>
2021-02-01 00:57:13 +0530

    Some changes in alpha.cpp

--- PROJECT/alpha.cpp
+++ PROJECT/alpha.cpp
@@ -4,9 +4,13 @@
 #include <sstream>
 #include <vector>
 #include <fstream>
-#include<map>
-using namespace std;
+#include <map>
+#include <random>
+#include <algorithm>
+#include <climits>
+#include<cstring>
 
+using namespace std;
 
 vector<string> timestamp;
 vector<string> username;
@@ -17,115 +21,265 @@ vector<string> semester;
 vector<string> company;
 vector<string> dslevel;
 vector<string> city;
+int no_of_users = 0;
 
-
-void write_pair(string user1,string user2){
-    string s="{"+user1+","+user2+"}\n";
+void write_pair(string user1, string user2)
+{
+    string s = "{" + user1 + "," + user2 + "}\n";
     ofstream op;
-    op.open("output.txt",ios::app);
-    if(op.is_open()){
-        op<<s;
+    op.open("output.txt", ios::app);
+    if (op.is_open())
+    {
+        op << s;
     }
-    else{
-        cout<<"Not able to open output.txt";
+    else
+    {
+        cout << "Not able to open output.txt";
     }
     op.close();
 }
 
-void delete_content(){
+void delete_content()
+{
     ofstream ofs;
     ofs.open("output.txt", std::ofstream::out | std::ofstream::trunc);
     ofs.close();
 }
 
-map<string,string> getinfo(vector<std::string> &input, std::string searched) {
-    
-    map <string,string> M;
+map<string, string> getinfo(vector<std::string> &input, std::string searched)
+{
+
+    map<string, string> M;
     int i;
-    for (i = 0; i < input.size(); i++) {
-        if (input[i] == searched) {
-            M["Timestamp"]=timestamp[i];
-            M["Username"]=username[i];
-            M["FaceoffId"]=faceoffId[i];
-            M["Fullname"]=fullname[i];
-            M["College"]=college[i];
-            M["Semester"]=semester[i];
-            M["Company"]=company[i];
-            M["Dslevel"]=dslevel[i];
-            M["City"]=city[i];
+    for (i = 0; i < input.size(); i++)
+    {
+        if (input[i] == searched)
+        {
+            M["Timestamp"] = timestamp[i];
+            M["Username"] = username[i];
+            M["FaceoffId"] = faceoffId[i];
+            M["Fullname"] = fullname[i];
+            M["College"] = college[i];
+            M["Semester"] = semester[i];
+            M["Company"] = company[i];
+            M["Dslevel"] = dslevel[i];
+            M["City"] = city[i];
             return M;
         }
     }
-    if(i==input.size()){
-        cout<<"Username does not exist";
+    if (i == input.size())
+    {
+        cout << "Username does not exist";
     }
     return M;
 }
 
-int main() {
+vector<string> split_string(string S)
+{
 
-	string v1,v2,v3,v4,v5,v6,v7,v8,v9; //variables from file are here
-	int no_of_users = 0;
+    char *s;
+    s = &S[0];
+    vector<string> str;
+    char *token = strtok(s, " ");
 
-	ifstream ip("input.csv"); //opening the file.
-	if (ip.is_open()) //if the file is open
-	{
-		//ignores the first line
-		string line;
-		getline(ip, line);
+    while (token != NULL)
+    {
+        string token_string(token);
 
-		while (!ip.eof()) //while the end of file is NOT reached
-		{
-			getline(ip, v1, ',');
-			timestamp.push_back(v1);
+        str.push_back(token_string);
 
-			getline(ip, v2, ',');
-			username.push_back(v2);
+        token = strtok(NULL, " ");
+    }
+    return str;
+}
 
-			getline(ip, v3, ',');
-			faceoffId.push_back(v3);
+void aayushiTask()
+{
+    vector<string> U;
 
-			getline(ip, v4, ',');
-			fullname.push_back(v4);
+    for (int i = 0; i < no_of_users; i++)
+    {
 
-			getline(ip, v5, ',');
-			college.push_back(v5);
+        U.push_back(username[i]);
+    }
 
-			getline(ip, v6, ',');
-			semester.push_back(v6);
+    shuffle(U.begin(), U.end(), mt19937(random_device()()));
 
-			getline(ip, v7, ',');
-			company.push_back(v7);
-
-			getline(ip, v8, ',');
-			dslevel.push_back(v8);
-
-			getline(ip, v9, '\n');
-			city.push_back(v9);
-
-			no_of_users += 1; //increment number of lines
-		}
-		ip.close(); //closing the file
-		no_of_users--;
-		cout << "Number of entries: " << no_of_users << endl;
-	}
-	else cout << "Unable to open file"; //if the file is not open output
+    vector<string> full;
+    full.reserve(U.size());
+    full.insert(full.end(), U.begin(), U.end());
+    if (full.size() % 2 == 0)
+    {
+        for (int i = 0; i < full.size(); i = i + 2)
+        {
+            write_pair(full[i], full[i + 1]);
+            // cout << "{" << full[i] << ", " << full[i + 1] << "}" << endl;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < full.size() - 3; i = i + 2)
+        {
+            write_pair(full[i], full[i + 1]);
+            // cout << "{" << full[i] << ", "  << full[i+1] << "}" << endl;
+        }
+        string last1 = full.back();
+        full.pop_back();
+        string last2 = full.back();
+        full.pop_back();
+        string last3 = full.back();
+        full.pop_back();
+        // cout << "{" << last3 << ", " << last2  <<  ", " << last1 << " }" << endl;
+    }
+}
+ 
+void ShivaniTask(){
     
-    //To remove the text of the file output.txt
-    delete_content();
-    
+}
+
+void ShwetaTask()
+{
+    for (auto &i : college)
+    {
+        if (i == "Shri Ram Murti Smarak College of Engg. and Tech" || i == "SRMS CET")
+            i = "SRMS";
+        else if (i == "IIT Delhi" || i == "Indian insstitute of technology Delhi")
+            i = "IIT D";
+        else if (i == "IIIT Delhi" || i == "Indian insstitute of information technology Delhi")
+            i = "IIIT D";
+        else if (i == "NIT Agartala")
+            i = "NITAgt";
+        else if (i == "NIT Allahabad")
+            i = "NITA";
+    }
+
+    vector<string> s;
+
+    for (auto &i : college)
+    {
+
+        transform(i.begin(), i.end(), i.begin(), ::toupper);
+    }
+
+    int n = college.size();
+
+    bool vis[500];
+    for (int i = 0; i < n; i++)
+    {
+        vis[i] = false;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (vis[i])
+            continue;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (college[i] != college[j])
+            {
+                write_pair(username[i],username[j]);
+                // cout << username[i] << " " << username[j] << endl;
+                vis[j] = true;
+                vis[i] = true;
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    string v1, v2, v3, v4, v5, v6, v7, v8, v9; //variables from file are here
+
+    ifstream ip("input.csv"); //opening the file.
+    if (ip.is_open())         //if the file is open
+    {
+        //ignores the first line
+        string line;
+        getline(ip, line);
+
+        while (!ip.eof()) //while the end of file is NOT reached
+        {
+            getline(ip, v1, ',');
+            timestamp.push_back(v1);
+
+            getline(ip, v2, ',');
+            username.push_back(v2);
+
+            getline(ip, v3, ',');
+            faceoffId.push_back(v3);
+
+            getline(ip, v4, ',');
+            fullname.push_back(v4);
+
+            getline(ip, v5, ',');
+            college.push_back(v5);
+
+            getline(ip, v6, ',');
+            semester.push_back(v6);
+
+            getline(ip, v7, ',');
+            company.push_back(v7);
+
+            getline(ip, v8, ',');
+            dslevel.push_back(v8);
+
+            getline(ip, v9, '\n');
+            city.push_back(v9);
+
+            no_of_users += 1; //increment number of lines
+        }
+        ip.close(); //closing the file
+        no_of_users--;
+        // cout << "Number of entries: " << no_of_users << endl;
+    }
+    else
+        cout << "Unable to open file"; //if the file is not open output
 
     //Start editing the code form here onwards if needed.....
 
     //To push the pair into the output.txt file
-    write_pair(username[0],username[1]);
 
     // To get the key value pairs of the passed username in the function getinfo()
-    map <string,string> M=getinfo(username,"codeogeek");
-    for( auto it = M.begin(); it != M.end(); ++it )
+    map<string, string> M = getinfo(username, "codeogeek");
+
+    while (1)
     {
-      cout<<it->first <<" : "<<it->second<<endl;
-    } 
-    
-	return 0;
+        
+        int x;
+        cout << "Input 1 for Aayushi's task:\n";
+        cout << "Input 2 for Aayushi's task:\n";
+        cout << "Input 3 for Shweta's task:\n";
+        cin >> x;
+        switch (x)
+        {
+        case 1:
+            delete_content();
+            aayushiTask();
+            cout << "See output.txt for the output..\n" << endl;
+            break;
+        case 2:
+            delete_content();
+            ShivaniTask();
+            cout << "See output.txt for the output..\n" << endl;
+            break;
+        case 3:
+            delete_content();
+            ShwetaTask();
+            cout << "See output.txt for the output..\n" << endl;
+            break;    
+        case -1:
+            exit(-1);
+            break;
+        default:
+            cout << "OOPS wrong input..\n" << endl;
+        }
+    }
+
+    for (auto it = M.begin(); it != M.end(); ++it)
+    {
+        //   cout<<it->first <<" : "<<it->second<<endl;
+    }
+
+    return 0;
 }
