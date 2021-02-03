de31e9e72d2bbdc5014f6f1dea1aafe26aaa594b
SHIVANI SUNDRIYAL <72017510+ShivaniSundriyal@users.noreply.github.com>
2021-02-03 18:33:13 +0530

    Update Shivani(timestramp).cpp

--- PROJECT/Shivani(timestramp).cpp
+++ PROJECT/Shivani(timestramp).cpp
@@ -15,12 +15,13 @@ int main()
     vector<string> dslevel;
     vector<string> city;
 
-    vector<pair<int,bool>> registrationSeconds;
+    vector<pair<int,string>> data;
+    vector<int> t1;
 
     string v1, v2, v3, v4, v5, v6, v7, v8, v9;
     int no_of_users = 0;
 
-    ifstream ip("C:/Users/shivani/Desktop/abc.csv");
+    ifstream ip("C:/Users/SHIVANI/Desktop/abc.csv");
     if (ip.is_open())
         {
         string line;
@@ -58,10 +59,10 @@ int main()
 
             int d, yr, h, m, s;
             string mon;
-            sscanf(v1.c_str(), "%d %s %d %d:%d:%d", &d, &mon, &yr, &h, &m, &s);
+            sscanf(v1.c_str(), "%d %s %d %d:%d:%d", &d, &mon, &yr, &h, &m, &s);//Time to seconds
             int secs = h * 3600 + m * 60 + s;
 
-            registrationSeconds.push_back(make_pair(secs,false));
+            t1.push_back(secs);
         }
         ip.close();
         cout << "Number of entries: " << no_of_users << endl;
@@ -70,22 +71,64 @@ int main()
     {
         cout << "Unable to open file";
     }
+    for(int i=0;i<t1.size();i++)
+    {
+        data.push_back({t1[i],username[i]});
+    }
 
-    for (int i = 0; i < registrationSeconds.size(); i++)
+    vector<string> pairs,single;
+
+    for(int i=0;i<data.size()-1;i++)
+    {
+        for(int j=1;j<data.size();j++)
         {
-          for(int j = 0; j < registrationSeconds.size(); j++)
-          {
-            if(!registrationSeconds[j].second and registrationSeconds[i].first + 900 >= registrationSeconds[j].first and i!=j)
+            if(data[j].first==0 || data[i].first==0)
             {
-                registrationSeconds[j].second = true;
-                registrationSeconds[i].second = true;
-
-                cout << "{ " << username[i] << ", " << username[j] << "}" << endl;
+                continue;
+            }
+            if(data[i].first - data[j].first>=900)
+            {
+                pairs.push_back(data[i].second);
+                pairs.push_back(data[j].second);
+                data[i].first=0;
+                data[j].first=0;
                 break;
+            }
+            else if(j==data.size()-1)
+            {
+                single.push_back(data[i].second);
             }
         }
     }
 
-    return 0;
+    for(int i=0;i<pairs.size()-2;i=i+2)
+    {
+        cout<<"{"<<pairs[i]<<","<<pairs[i+1]<<"}\n";
+    }
+    cout<<"\n";
+    /*
+    if(single.size()%2==0)
+    {
+        for(int i=0;i<single.size()-2;i=i+2)
+        {
+            cout<<"{"<<single[i]<<","<<single[i+1]<<"}\n";
+        }
+    }
+    else
+    {
+       int var=(single.size()/2-1)*2;
+       for(int i=0;i<single.size()-1;i=i+2)
+       {
+           if(i<var)
+           {
+               cout<<"{"<<single[i]<<","<<single[i+1]<<"}\n";
+           }
+           else
+           {
+               cout<<"{"<<single[i]<<","<<single[i+1]<<","<<single.back()<<"}\n";
+           }
+       }
+    }*/
 
+    return 0;
 }
